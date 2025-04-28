69bae2d0823113084274a2fc97d9dc560e50069d
varadrane1707 <varad@simplismart.tech>
2025-04-28 17:04:03 +0530

    cuda issue resolved

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ pip3 install 'accelerate>=0.26.0'
 pip3 install protobuf
 pip3 install sentencepiece
 pip3 install pydantic
-pip3 install torchao
+pip3 uninstall torch torchvision torchaudio
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 && pip3 install torchao
